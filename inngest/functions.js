523ffbee8d9ae86e2fecb36f1bd480a1d9fc4252
Vedant Chaudhari <vedantm606@gmail.com>
2025-09-09 15:19:25 +0530

    correction in syncUserCreation function

--- inngest/functions.js
+++ inngest/functions.js
@@ -10,7 +10,7 @@ export const syncUserCreation = inngest.createFunction(
             data: {
                 id: data.id,
                 email: data.email_addresses[0].email_address,
-                name: `${data.first_name} ${data/last_name}`,
+                name: `${data.first_name} ${data.last_name}`,
                 image: data.image_url
             }
         })
