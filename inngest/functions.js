4bc1e43168c9f063e1bc069a03a643c644c142bb
Vedant Chaudhari <vedantm606@gmail.com>
2025-09-14 19:18:28 +0530

    some changes in inngest functions file

--- inngest/functions.js
+++ inngest/functions.js
@@ -50,7 +50,7 @@ export const deleteCouponOnExpiry = inngest.createFunction(
     { event: 'app/coupon.expired' },
     async ({ event, step }) => {
         const { data } = event;
-        const expiryDate = new Date(Date.expires_at)
+        const expiryDate = new Date(data.expires_at)
         await step.sleepUntil('wait-for-expiry', expiryDate)
 
         await step.run('delete-coupon-from-database', async () => {
